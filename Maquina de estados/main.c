6a95db66b4e4ffcc22e9dfa3e2335e4166e5e7d5
davidmolecula <68165224+davidmolecula@users.noreply.github.com>
2021-02-20 21:40:16 -0300

    Update main.c

--- Maquina de estados/main.c
+++ Maquina de estados/main.c
@@ -11,7 +11,7 @@
 int main() {
 	datos_t config; //Declaro la estructura donde van a estar los datos que necesita la maquina 
 	estados_t estado = okay; //inicializo la maquina con
-    estados_t (*fsm[])(datos_t) = {f_desconexion, f_espera};/* Declaro el vector puntero a funciones que va a llamar a las funciones y pasarles la 
+    estados_t (*fsm[])(datos_t) = {f_espera, f_desconexion};/* Declaro el vector puntero a funciones que va a llamar a las funciones y pasarles la 
 	estructura segun el estado en el que se encuentre la maquina */
     config=f_inicio(); // Llamo a la funcion inicio que será la que lea el archivo de configuracion y establezca el estado de la maquina
     while(1) estado = (*fsm[estado])(config);
