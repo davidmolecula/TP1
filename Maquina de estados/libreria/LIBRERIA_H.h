3c4128f002abd55d4a42c4006fe0565684ef142c
davidmolecula <68165224+davidmolecula@users.noreply.github.com>
2021-02-23 22:19:19 -0300

    Add files via upload

---
+++ Maquina de estados/libreria/LIBRERIA_H.h
@@ -0,0 +1,34 @@
+#ifndef LIBRERIA_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef enum {
+	okay=0,
+	notokay=1
+}estados_t;
+
+typedef enum{
+	cables=0,
+	tension=1,
+	tension_limite=2,
+	avisos=3
+}claves_t;
+
+typedef struct {
+ estados_t cables;
+ int tension_limite;
+ int tension;
+ char avisos[50];	
+}datos_t;
+
+datos_t f_inicio(void);
+estados_t f_espera(datos_t);
+estados_t f_desconexion(datos_t);
+int f_medir_tension();
+estados_t f_estados(int, int );
+char *obtenerclave(char *clave);
+
+#endif
